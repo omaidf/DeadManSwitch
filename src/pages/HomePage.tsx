cdfe315d1ce480bb9f0e2160aba6a13e5ce45ff4
0xWizz <11565994+0xWizzzz@users.noreply.github.com>
2025-08-03 15:26:03 -0700

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react'
 import { Link } from 'react-router-dom'
 import { useWallet } from '@solana/wallet-adapter-react'
 import { NetworkStatus } from '../components/NetworkStatus'
-import { FaGithub, FaTwitter, FaFilePdf } from 'react-icons/fa'
 
 /**
  * Landing page component that introduces the Dead Man's Switch application.
@@ -81,13 +80,26 @@ export const HomePage: FC = () => {
         </p>
         <div className="flex justify-center space-x-8">
           <a href="https://github.com/0xWizzzz/DeadManSwitch" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors">
-            <FaGithub className="w-10 h-10 text-purple-400 hover:text-purple-300" />
+            <div className="w-10 h-10 text-purple-400 hover:text-purple-300 flex items-center justify-center">
+              <svg className="w-8 h-8 fill-current" viewBox="0 0 24 24">
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+              </svg>
+            </div>
           </a>
           <a href="https://x.com/deadmanonsol" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors">
-            <FaTwitter className="w-10 h-10 text-sky-400 hover:text-sky-300" />
+            <div className="w-10 h-10 text-sky-400 hover:text-sky-300 flex items-center justify-center">
+              <svg className="w-8 h-8 fill-current" viewBox="0 0 24 24">
+                <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/>
+              </svg>
+            </div>
           </a>
           <a href="/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors">
-            <FaFilePdf className="w-10 h-10 text-red-400 hover:text-red-300" />
+            <div className="w-10 h-10 text-red-400 hover:text-red-300 flex items-center justify-center">
+              <svg className="w-8 h-8 fill-current" viewBox="0 0 24 24">
+                <path d="M8.267 14.68c-.184 0-.308.018-.372.036v1.178c.076.018.171.023.302.023.479 0 .774-.242.774-.651 0-.366-.254-.586-.704-.586zm3.487.012c-.2 0-.33.018-.407.036v2.61c.077.018.201.018.313.018.817.006 1.349-.444 1.349-1.396.006-.83-.479-1.268-1.255-1.268z"/>
+                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zM9.498 16.19c-.309.29-.765.42-1.296.42a2.23 2.23 0 0 1-.308-.018v1.426H7v-3.936A7.558 7.558 0 0 1 8.219 14c.557 0 .953.106 1.22.319.254.202.426.533.426.923-.001.392-.131.723-.367.948zm3.807 1.355c-.42.349-1.059.515-1.84.515-.468 0-.799-.03-1.024-.06v-3.917A7.947 7.947 0 0 1 11.66 14c.757 0 1.249.136 1.633.426.415.308.675.799.675 1.504 0 .763-.279 1.29-.663 1.615zM17 14.77h-1.532v.911H16.9v.734h-1.432v1.604h-.906V14.03H17v.74zM14 9h-1V4l5 5h-4z"/>
+              </svg>
+            </div>
           </a>
         </div>
       </div>
